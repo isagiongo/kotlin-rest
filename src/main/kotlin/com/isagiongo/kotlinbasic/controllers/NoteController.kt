698972747aaa3823177b2bfafef1537f62d5225b
Isa Giongo <isagiongo@gmail.com>
2019-05-23 19:56:48 -0300

    Adiciona metodo add no controller

--- src/main/kotlin/com/isagiongo/kotlinbasic/controllers/NoteController.kt
+++ src/main/kotlin/com/isagiongo/kotlinbasic/controllers/NoteController.kt
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.GetMapping
 import com.isagiongo.kotlinbasic.models.Note
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 
 @RestController
 @RequestMapping("notes")
@@ -12,5 +14,10 @@ class NoteController {
 	@GetMapping
 	fun list(): List<Note>{
 		return listOf(Note("Leitura", "Livro de Spring Boot"), Note("Pesquisa","Angular Básico"))
+	}
+	
+	@PostMapping
+	fun add(@RequestBody note: Note): Note{
+		return note
 	}
 }
--- src/main/kotlin/com/isagiongo/kotlinbasic/models/Note.kt
+++ src/main/kotlin/com/isagiongo/kotlinbasic/models/Note.kt
@@ -1,3 +1,3 @@
 package com.isagiongo.kotlinbasic.models
 
-data class Note (val title: String, val description: String)
+data class Note (val title: String = "", val description: String = "")
